b2ddcebf6d71683101e4724a604a8e3516bbc0be
Sprintermax <ericfnoe7@hotmail.com>
2022-09-28 12:56:29 -0300

    image script improvements

--- src/images.js
+++ src/images.js
@@ -21,23 +21,32 @@ for (const islandCode of islandCodes) {
     getIsland(islandCode);
 }
 
-async function getIsland(islandCode) {
-    const res = await fetch(`https://fortniteapi.io/v1/creative/island?code=${islandCode.split('?')[0]}`, {
-        headers: {
-            Authorization: process.env.API_TOKEN
-        }
-    });
-    if (res.status === 429) {
-        console.log(`Ratelimited when getting "${islandCode}" data, trying again...`);
-        setTimeout(() => getIsland(islandCode), 100);
-    } else if (res.ok) {
-        const islandData = await res.json();
-        if (islandData.island) {
-            const islandImage = await fetch(islandData.island.image || islandData.island.promotion_image);
-            if (islandImage) {
-                islandImage.body.pipe(fs.createWriteStream(`./images/${islandCode.replace('?', '_').replace('=', '')}.png`));
-                console.log(`Saving island image for code "${islandCode}"`);
-            } else console.log(`No island image found for code "${islandCode}", skipping`);
-        } else console.log(`No island data found for code "${islandCode}", skipping`);
-    } else console.log(`An error ocurred when trying to get island data for code "${islandCode}", skipping`);
+async function getIsland(islandCode, retry = 0) {
+    try {
+        const res = await fetch(`https://fortniteapi.io/v1/creative/island?code=${islandCode.split('?')[0]}`, {
+            headers: {
+                Authorization: process.env.API_TOKEN
+            }
+        });
+        if (res.status === 429) {
+            retry++;
+            if (retry > 50) return console.log(`Max rate limit tries reached when fetching "${islandCode}" data, skipping.`);
+            console.log(`Ratelimited when fetching "${islandCode}" data, trying again (${retry}/50)...`);
+            setTimeout(() => getIsland(islandCode, retry), 250);
+        } else if (res.ok) {
+            const islandData = await res.json();
+            if (islandData.island) {
+                const islandImage = await fetch(islandData.island.image || islandData.island.promotion_image);
+                if (islandImage) {
+                    islandImage.body.pipe(fs.createWriteStream(`./images/${islandCode.replace('?', '_').replace('=', '')}.png`));
+                    console.log(`Saving island image for code "${islandCode}"`);
+                } else console.log(`No island image found for code "${islandCode}", skipping.`);
+            } else console.log(`No island data found for code "${islandCode}", skipping.`);
+        } else console.log(`An error ocurred when trying to get island data for code "${islandCode}", skipping.`);
+    } catch (e) {
+        retry++;
+        if (retry > 3) return console.log(`Max attempts reached when fetching "${islandCode}" data, skipping.`);
+        console.log(`Unexpected error when fetching "${islandCode}" data, trying again (${retry}/3)...`)
+        setTimeout(() => getIsland(islandCode, retry), 100);
+    }
 }
